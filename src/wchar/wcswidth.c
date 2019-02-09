452ac2f7d9d5ecd57e80d95e5752f8c8304ff0b0
Jakob Kaivo <jkk@ung.org>
2019-02-09 15:08:15 -0500

    merge XOPEN identifiers

---
+++ src/wchar/wcswidth.c
@@ -0,0 +1,10 @@
+#include <wchar.h>
+
+int wcswidth(const wchar_t * wcsptr, size_t n)
+{
+}
+
+/*
+XOPEN(4)
+NOTE: this portion is shaded WP in SUSv1
+*/
---
+++ src/wchar/wcwidth.c
@@ -0,0 +1,10 @@
+#include <wchar.h>
+
+int wcwidth(wchar_t wc)
+{
+}
+
+/*
+XOPEN(4)
+NOTE: this function is shaded "WP" in SUSv1
+*/
