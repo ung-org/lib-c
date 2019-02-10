d1e7a840fa4f143a0f2ff795bc18a48b1351c74c
Jakob Kaivo <jkk@ung.org>
2019-02-10 14:56:36 -0500

    add missing symbols

--- src/sys/wait/idtype_t.c
+++ src/sys/wait/idtype_t.c
@@ -1,5 +1,9 @@
 #include <sys/wait.h>
-typedef enum { P_ALL, P_PGID, P_PID }                                  idtype_t;
+typedef enum {
+	P_ALL,
+	P_PGID,
+	P_PID
+} idtype_t;
 /*
 XOPEN(400)
 POSIX(200809)
