803354bd8611ca7ff3fa34752aaba4981a4d3ef9
Jakob Kaivo <jkk@ung.org>
2019-02-10 14:05:57 -0500

    add missing symbols

---
+++ src/unistd/_SC_2_C_BIND.c
@@ -0,0 +1,6 @@
+#include <unistd.h>
+#define _SC_2_C_BIND                                                           1
+/*
+POSIX(2)
+*/
+
---
+++ src/unistd/_SC_2_C_VERSION.c
@@ -0,0 +1,6 @@
+#include <unistd.h>
+#define _SC_2_C_VERSION                                                        1
+/*
+POSIX(2)
+*/
+
