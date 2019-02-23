78aa2fc7871a4f79613d38122dc6d5ac7454f57c
Jakob Kaivo <jkk@ung.org>
2019-02-23 15:21:37 -0500

    merge SYSCALL macros into one

--- src/nonstd/SYSCALL.c
+++ src/nonstd/SYSCALL.c
@@ -1,11 +1,11 @@
 #include <nonstd/syscall.h>
 
-#define SYSCALL(_name, _type, _args) \
+#define SYSCALL(_name, _type, _err, _a1, _a2, _a3, _a4, _a5, _a6) \
 	static int _scno = -2; \
 	if (_scno == -2) { _scno = ((syscall_lookup_t)__libc(SYSCALL_LOOKUP))(_name); } \
-	_type _ret = __syscall(_scno, _args); \
+	long _ret = __syscall(_scno, _a1, _a2, _a3, _a4, _a5, _a6); \
 	if (_ret < 0) { \
 		errno = -_ret; \
-		return -1; \
+		return _err; \
 	} \
-	return _ret
+	return (_type)_ret
--- src/nonstd/SYSCALL_NOFAIL.c
+++
@@ -1,6 +0,0 @@
-#include <nonstd/syscall.h>
-
-#define SYSCALL_NOFAIL(_name) \
-	static int _scno = -2; \
-	if (_scno == -2) { _scno = __libc(SYSCALL_LOOKUP)(_name); } \
-	return __libc(SYSCALL)(_scno)
