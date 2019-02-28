6a6c33f194a2de8f1c12a4cfd74ad5ad393808e6
Jakob Kaivo <jkk@ung.org>
2019-02-27 19:19:42 -0500

    set to correct value

--- src/stdint/UINTMAX_MAX.c
+++ src/stdint/UINTMAX_MAX.c
@@ -1,5 +1,5 @@
 #include <stdint.h>
-#define UINTMAX_MAX                                    (18446744073709551616ULL)
+#define UINTMAX_MAX                                    (18446744073709551615ULL)
 
 /*
 STDC(199901)
