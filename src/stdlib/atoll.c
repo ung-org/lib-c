9f7b74c64bf2730c2a23e66693b49bd98e5bf28b
Jakob Kaivo <jkk@ung.org>
2019-03-02 15:02:56 -0500

    add documentation

--- src/stdlib/atoll.c
+++ src/stdlib/atoll.c
@@ -1,10 +1,19 @@
 #include <stdlib.h>
 
+/** convert string to long long integer **/
+
 long long int atoll(const char *nptr)
 {
 	return strtoll(nptr, (char**)NULL, 10);
 }
 
+/***
+converts the string at ARGUMENT(nptr) to a TYPE(long long int) value,
+using base 10. The conversion goes until the first non-digit character.
+***/
+
 /*
+LC_CTYPE
+RETURN_SUCCESS(the converted value)
 STDC(199901)
 */
