c22a0473c06092a41fb728ce668572082bb50636
Jakob Kaivo <jkk@ung.org>
2019-03-02 11:17:17 -0500

    update documentation

--- src/errno/EILSEQ.c
+++ src/errno/EILSEQ.c
@@ -1,7 +1,10 @@
 #include <errno.h>
 #define EILSEQ                                                               (3)
-/** Illegal sequence / Illegal byte sequence **/
-
+/** illegal sequence **/
+/***
+is used to indicate that an incorrect sequence of bytes was encountered when
+transforming a multi-byte character into a wide character.
+***/
 /*
 STDC(199409)
 */
--- src/errno/ERANGE.c
+++ src/errno/ERANGE.c
@@ -2,7 +2,7 @@
 #define ERANGE                                                               (2)
 /** out of range **/
 /***
-used to indicated that the result of an operation is too large or too small.
+is used to indicated that the result of an operation is too large or too small.
 ***/
 /*
 STDC(1)
