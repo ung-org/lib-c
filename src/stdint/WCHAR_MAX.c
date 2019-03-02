272de2498a02faa16098ed76faa1a5c3842902db
Jakob Kaivo <jkk@ung.org>
2019-03-01 21:22:52 -0500

    these should be references to the <wchar.h> definition

--- src/stdint/WCHAR_MAX.c
+++
@@ -1,6 +0,0 @@
-#include <stdint.h>
-#define WCHAR_MAX                                                     (0x10FFFF)
-
-/*
-STDC(199901)
-*/
--- src/stdint/WCHAR_MIN.c
+++
@@ -1,6 +0,0 @@
-#include <stdint.h>
-#define WCHAR_MIN                                                            (0)
-
-/*
-STDC(199901)
-*/
