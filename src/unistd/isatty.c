26eb259d69ca2e8be905349c930818756f404601
Jakob Kaivo <jkk@ung.org>
2019-03-02 15:03:13 -0500

    document and implement in terms of tcgetattr()

--- src/unistd/isatty.c
+++ src/unistd/isatty.c
@@ -1,13 +1,31 @@
 #include "stddef.h"
 #include "sys/types.h"
 #include <unistd.h>
-#include "errno.h"
-#include "nonstd/syscall.h"
+#include "termios.h"
+
+/** test for a terminal device **/
 
 int isatty(int fildes)
 {
-	SYSCALL("isatty", int, -1, fildes, 0, 0, 0, 0, 0);
+	struct termios tios;
+	if (tcgetattr(fildes, &tios) == 0) {
+		/* errno set by tcgetattr() */
+		#if 0
+		errno = EBADF;	/* ARGUMENT(fildes) is not a valid file descriptor */
+		errno = ENOTTY;	/* ARGUMENT(fildes) is not associated with a terminal */
+		#endif
+		return 1;
+	}
+	return 0;
 }
+
+/***
+checks whether the file descriptor ARGUMENT(fildes) is associated with a
+terminal device.
+***/
+
 /*
+RETURN(ZERO, ARGUMENT(fildes) is not a terminal)
+RETURN(ONE, ARGUMENT(fildes) is a terminal device)
 POSIX(1)
 */
