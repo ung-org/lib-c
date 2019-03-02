0db68d240e1223df4310b8c21061eb6e523d7a49
Jakob Kaivo <jkk@ung.org>
2019-03-01 21:23:11 -0500

    add the missing 0

--- src/wchar/WCHAR_MAX.c
+++ src/wchar/WCHAR_MAX.c
@@ -1,5 +1,5 @@
 #include <wchar.h>
-#define WCHAR_MAX                                                      (0x1FFFF)
+#define WCHAR_MAX                                                     (0x10FFFF)
 
 /*
 STDC(199409)
