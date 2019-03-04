f20eeea657d62f2ef905627ca3a9094d33af7d40
Jakob Kaivo <jkk@ung.org>
2019-03-03 21:23:17 -0500

    reference TGFNs from __fpclassify

--- src/math/fpclassify.c
+++ src/math/fpclassify.c
@@ -1,10 +1,7 @@
 #include <math.h>
-                              int __fpclassifyf(float x);
-                             int __fpclassifyd(double x);
-                        int __fpclassifyl(long double x);
 
 #define fpclassify(x)    ((sizeof (x) == sizeof (float)) ? __fpclassifyf(x) : \
-	(sizeof (x) == sizeof (double)) ? __fpclassifyd(x) :  __fpclassifyl(x))
+	(sizeof (x) == sizeof (double)) ? __fpclassify(x) :  __fpclassifyl(x))
 
 /*
 STDC(199901)
