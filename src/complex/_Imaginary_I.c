7531afc84c78bce8dfaf199a72db5d63ded16a30
Jakob Kaivo <jkk@ung.org>
2019-03-03 21:18:51 -0500

    get around to implementing

--- src/complex/_Imaginary_I.c
+++ src/complex/_Imaginary_I.c
@@ -1,10 +1,15 @@
 #include <complex.h>
 
 #ifdef __STDC_IEC_559_COMPLEX__
-#define _Imaginary_I        (const float _Imaginary)1 /* TODO: imaginary unit */
+#define _Imaginary_I \
+	(((union { \
+		float _Imaginary __i; \
+		float __f; \
+	}){ .__f = 1.0 }).__i)
 #endif
 
 /* This should only be defined if imaginary types are supported. */
+
 /*
 STDC(199901)
 */
