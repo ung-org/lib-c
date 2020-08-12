556843458a3b38f1efe536fdb7ff3b0f65538e0f
Jakob Kaivo <jkk@ung.org>
2020-08-12 16:09:32 -0400

    fix to be solf-contained and more reliable

--- src/string/strcmp.c
+++ src/string/strcmp.c
@@ -4,21 +4,32 @@
 /** compare strings **/
 int strcmp(const char *s1, const char *s2)
 {
-	size_t n1 = 0;
-	size_t n2 = 0;
-
 	ASSERT_NONNULL(s1);
 	ASSERT_NONNULL(s2);
 
-	n1 = strlen(s1);
-	n2 = strlen(s2);
+	while (*s1 && *s2) {
+		if (*s1 != *s2) {
+			return *s1 - *s2;
+		}
+		s1++;
+		s2++;
+	}
 
 	/*
 	RETURN(NEGATIVE, ARGUMENT(s1) is less than ARGUMENT(s2));
 	RETURN(ZERO, ARGUMENT(s1) is equal to ARGUMENT(s2));
 	RETURN(POSITIVE, ARGUMENT(s1) is greater than ARGUMENT(s2));
 	*/
-	return strncmp(s1, s2, n1 < n2 ? n1 : n2);
+
+	if (*s1) {
+		return -1;
+	}
+
+	if (*s2) {
+		return 1;
+	}
+
+	return 0;
 }
 
 /***
