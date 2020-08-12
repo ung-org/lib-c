158922e3403b664da74821138f3be061e23a5fe8
Jakob Kaivo <jkk@ung.org>
2020-08-12 16:37:20 -0400

    implement as syscall

--- src/sys/stat/stat.c
+++ src/sys/stat/stat.c
@@ -2,30 +2,14 @@
 #include <sys/stat.h>
 #include "stdlib.h"
 #include "../../_assert.h"
+#include "../../_syscall.h"
 
 int stat(const char * restrict path, struct stat * restrict buf)
 {
 	ASSERT_NONNULL(path);
 	ASSERT_NONNULL(buf);
 
-	int ret = 0;
-	#if 0
-	char *linkbuf = NULL;
-
-	do {
-		ret = lstat(path, buf);
-		if (S_ISLNK(buf->st_mode)) {
-			linkbuf = realloc(linkbuf, buf->st_size + 1);
-			readlink(path, linkbuf, buf->st_size);
-			path = linkbuf;
-		} else {
-			path = NULL;
-		}
-	} while (path);
-	realloc(linkbuf, 0);
-	#endif
-
-	return ret;
+	SYSCALL(stat, int, -1, path, buf, 0, 0, 0, 0);
 }
 /*
 POSIX(1)
