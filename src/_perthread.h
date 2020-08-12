c53a55b6db53de6691660483e1a5d695a43ac090
Jakob Kaivo <jkk@ung.org>
2020-08-12 13:20:36 -0400

    add macro for use where thread local storage is required

---
+++ src/_perthread.h
@@ -0,0 +1,11 @@
+#ifndef ___PERTHREAD_H__
+#define ___PERTHREAD_H__
+
+#if defined __STDC_VERSION__ && 201112L <= __STDC_VERSION__ && !defined __STDC_N
+O_THREADS__
+#define THREAD_LOCAL static _Thread_local
+#else
+#define THREAD_LOCAL static
+#endif
+
+#endif
