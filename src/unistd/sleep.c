fe9e78411f270c8c86d75620bbaa53147b9c7daa
Jakob Kaivo <jkk@ung.org>
2020-08-12 15:14:06 -0400

    implement in terms of nanosleep()

--- src/unistd/sleep.c
+++ src/unistd/sleep.c
@@ -1,15 +1,24 @@
 #include "stddef.h"
 #include "sys/types.h"
 #include <unistd.h>
-#include "../_syscall.h"
+#include "time.h"
+
+#if _POSIX_C_SOURCE < 199309
+#define nanosleep __nanosleep
+#include "../time/struct_timespec.c"
+#include "../time/nanosleep.c"
+#endif
 
 unsigned sleep(unsigned seconds)
 {
-	#if 0
-	SC(unsigned, seconds);
-	#else
-	return seconds;
-	#endif
+	struct timespec tosleep = { seconds, 0 };
+	struct timespec remaining = { 0 , 0 };
+
+	if (nanosleep(&tosleep, &remaining) == -1) {
+		return seconds;
+	}
+
+	return remaining.tv_sec;
 }
 /*
 POSIX(1)
