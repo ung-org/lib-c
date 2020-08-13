f7b930d5218b09c7fcd61436dc815ca4987f9d7b
Jakob Kaivo <jkk@ung.org>
2020-08-13 10:57:23 -0400

    implement as syscall

--- src/sys/stat/mkfifo.c
+++ src/sys/stat/mkfifo.c
@@ -1,10 +1,14 @@
 #include "sys/types.h"
 #include <sys/stat.h>
+#include "../../_syscall.h"
+
+#ifndef S_IFIFO
+#include "S_IFIFO.c"
+#endif
 
 int mkfifo(const char *path, mode_t mode)
 {
-	(void)path; (void)mode;
-	return 0;
+	SYSCALL(mknod, int, -1, path, mode, S_IFIFO, 0, 0, 0);
 }
 /*
 POSIX(1)
