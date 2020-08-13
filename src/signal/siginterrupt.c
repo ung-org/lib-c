834a4930e8d9b33373fbe1d636acebb87b62c2fd
Jakob Kaivo <jkk@ung.org>
2020-08-13 19:45:54 -0400

    implement

--- src/signal/siginterrupt.c
+++ src/signal/siginterrupt.c
@@ -1,8 +1,18 @@
+#include "sys/types.h"
 #include <signal.h>
 
 int siginterrupt(int sig, int flag)
 {
-	return 0;
+	struct sigaction sa = { 0 };
+	int ret = -1;
+
+	sigaction(sig, NULL, &sa);
+	if (flag) {
+		sa.sa_flags &= ~SA_RESTART;
+	} else {
+		sa.sa_flags |= SA_RESTART;
+	}
+	return sigaction(sig, &sa, NULL);
 }
 
 /*
