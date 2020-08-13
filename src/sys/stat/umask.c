ea6629608487ef59074636ca32bd80bcd40368ff
Jakob Kaivo <jkk@ung.org>
2020-08-13 11:13:14 -0400

    implement as syscall

--- src/sys/stat/umask.c
+++ src/sys/stat/umask.c
@@ -1,9 +1,10 @@
 #include "sys/types.h"
 #include <sys/stat.h>
+#include "../../_syscall.h"
 
 mode_t umask(mode_t cmask)
 {
-	return cmask;
+	SYSCALL(umask, mode_t, -1, cmask, 0, 0, 0, 0, 0);
 }
 
 /*
