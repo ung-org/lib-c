e60d499c51d033447f21bb110c899cd2e578d065
Jakob Kaivo <jkk@ung.org>
2020-08-13 12:02:20 -0400

    implement as syscall

--- src/sys/mman/mmap.c
+++ src/sys/mman/mmap.c
@@ -1,8 +1,14 @@
+#include "sys/types.h"
 #include <sys/mman.h>
+#include "../../_syscall.h"
+
+#ifndef MAP_FAILED
+#include "MAP_FAILED.c"
+#endif
 
 void *mmap(void *addr, size_t len, int prot, int flags, int fildes, off_t off)
 {
-	return addr;
+	SYSCALL(mmap, void*, MAP_FAILED, addr, len, prot, flags, fildes, off);
 }
 
 /*
