57a91fc9f0eb8a14ce9ffa4d5218dc8e358fb273
Jakob Kaivo <jkk@ung.org>
2020-08-14 16:28:25 -0400

    reimplement in terms of getpwent()

--- src/pwd/getpwnam.c
+++ src/pwd/getpwnam.c
@@ -4,90 +4,21 @@
 #include "stdio.h"
 #include "limits.h"
 #include "string.h"
-#include "_config.h"
-
-#ifndef LINE_MAX
-#define LINE_MAX _POSIX2_LINE_MAX
-#endif
+#include "_pwd.h"
 
 struct passwd * getpwnam(const char * name)
 {
-	static char buf[LINE_MAX + 1];
-	static struct passwd pwd = { 0 };
-	char *user, *password, *uid, *gid, *gecos, *home, *shell, *nl;
+	struct passwd *pwd = NULL;
+	setpwent();
 
-	/* TODO: attempt calling first _PWD_CMD */
-
-	FILE *db = fopen(_PWD_DB, "r");
-	if (db == NULL) {
-		return NULL;
+	while ((pwd = getpwent()) != NULL) {
+		if (strcmp(name, pwd->pw_name) == 0) {
+			endpwent();
+			return pwd;
+		}
 	}
-
-	while (fgets(buf, sizeof(buf), db) != NULL) {
-		user = buf;
-		if ((password = strchr(buf, ':')) != NULL) {
-			*password = '\0';
-			password++;
-		} else {
-			continue;
-		}
-
-		if (strcmp(user, name) != 0) {
-			continue;
-		}
-
-		if ((uid = strchr(password, ':')) != NULL) {
-			*uid = '\0';
-			uid++;
-		} else {
-			continue;
-		}
-
-		if ((gid = strchr(uid, ':')) != NULL) {
-			*gid = '\0';
-			gid++;
-		} else {
-			continue;
-		}
-
-		if ((gecos = strchr(gid, ':')) != NULL) {
-			*gecos = '\0';
-			gecos++;
-		} else {
-			continue;
-		}
-
-		if ((home = strchr(gecos, ':')) != NULL) {
-			*home = '\0';
-			home++;
-		} else {
-			continue;
-		}
-
-		if ((shell = strchr(home, ':')) != NULL) {
-			*shell = '\0';
-			shell++;
-			if ((nl = strchr(shell, '\n')) != NULL) {
-				*nl = '\0';
-			}
-		} else {
-			continue;
-		}
-
-		pwd.pw_name = user;
-		pwd.pw_uid = strtoul(uid, NULL, 10);
-		pwd.pw_gid = strtoul(gid, NULL, 10);
-		pwd.pw_dir = home;
-		pwd.pw_shell = shell;
-		break;
-	}
-
-	fclose(db);
-
-	if (strcmp(name, pwd.pw_name) == 0) {
-		return &pwd;
-	}
-
+	
+	endpwent();
 	return NULL;
 }
 
