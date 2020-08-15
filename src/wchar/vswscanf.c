6228d597543fa2682374f950355fb31691cf930f
Jakob Kaivo <jkk@ung.org>
2020-08-15 15:28:16 -0400

    make static if compiling for C95

--- src/wchar/vswscanf.c
+++ src/wchar/vswscanf.c
@@ -1,5 +1,9 @@
 #include <wchar.h>
-#include "../stdio/_stdio.h"
+#include "stdio/_stdio.h"
+
+#if __STDC_VERSION__ < 199901L
+	static
+#endif
 
 int vswscanf(const wchar_t * restrict s, const wchar_t * restrict format, va_list arg)
 {
