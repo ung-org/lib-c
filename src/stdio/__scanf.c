dc0619b0df46739db4a82ea6068ae69289f6f693
Jakob Kaivo <jkk@ung.org>
2020-08-15 19:53:55 -0400

    add empty prototype function

---
+++ src/stdio/__scanf.c
@@ -0,0 +1,11 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "_stdio.h"
+
+int __scanf(struct io_options *opt, const char * format, va_list arg)
+{
+	(void)opt;
+	(void)format;
+	(void)arg;
+	return 0;
+}
