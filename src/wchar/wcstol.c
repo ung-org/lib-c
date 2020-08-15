55973790cab542b1f657b4fb1db45e9ee00c116a
Jakob Kaivo <jkk@ung.org>
2020-08-15 15:28:54 -0400

    cleanup headers

--- src/wchar/wcstol.c
+++ src/wchar/wcstol.c
@@ -1,7 +1,7 @@
 #include <wchar.h>
-#include "wctype.h"
-#include "limits.h"
-#include "errno.h"
+#include <wctype.h>
+#include <limits.h>
+#include <errno.h>
 
 #define isspace iswspace
 
@@ -11,7 +11,7 @@ long int wcstol(const wchar_t * restrict nptr, wchar_t ** restrict endptr, int b
 	long int max = LONG_MAX;
 	long int min = LONG_MIN;
 
-	#include "../stdlib/_strtoi.h"
+	#include "stdlib/_strtoi.h"
 
 	return ret;
 }
