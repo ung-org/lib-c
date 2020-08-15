87d5824b904c5f015f6988d11b04dafda355f9af
Jakob Kaivo <jkk@ung.org>
2020-08-15 15:43:57 -0400

    adjust to compile with c89

--- src/string/memccpy.c
+++ src/string/memccpy.c
@@ -2,16 +2,19 @@
 
 void *memccpy(void * restrict s1, const void * restrict s2, int c, size_t n)
 {
-	//__ASSERT_NOOVERLAP(s1, s2, n);
-
 	unsigned char *dst = s1;
 	const unsigned char *src = s2;
-	for (size_t i = 0; i < n; i++) {
+	size_t i = 0;
+
+	ASSERT_NOOVERLAP(s1, s2, n);
+
+	for (i = 0; i < n; i++) {
 		dst[i] = src[i];
 		if (dst[i] == (unsigned char)c) {
 			return dst + i + 1;
 		}
 	}
+
 	return NULL;
 }
 
