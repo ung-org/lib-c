022c0e3f552a1973abc35ff8bea657a9bc4ade56
Jakob Kaivo <jkk@ung.org>
2020-08-15 15:24:49 -0400

    implement

--- src/wchar/fputws.c
+++ src/wchar/fputws.c
@@ -1,10 +1,16 @@
 #include <wchar.h>
-#include "stdio.h"
+#include <stdio.h>
 
 int fputws(const wchar_t * restrict s, FILE * restrict stream)
 {
-	(void)s; (void)stream;
-	return -1;
+	const wchar_t *p = s;
+	while (*p != L'\0') {
+		if (fputwc(*p, stream) == EOF) {
+			return EOF;
+		}
+		p++;
+	}
+	return 0;
 }
 
 /*
