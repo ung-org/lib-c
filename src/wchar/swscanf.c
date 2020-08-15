61e4ad4901b23e7dc21bd8fda0f81fc3b283389e
Jakob Kaivo <jkk@ung.org>
2020-08-15 15:26:09 -0400

    implement

--- src/wchar/swscanf.c
+++ src/wchar/swscanf.c
@@ -1,17 +1,18 @@
+#include <stdarg.h>
 #include <wchar.h>
-#include "stdarg.h"
+
+#if __STDC_VERSION__ < 199901L
+#define vswscanf __vswscanf
+#include "vswscanf.c"
+#endif
 
 int swscanf(const wchar_t * restrict s, const wchar_t * restrict format, ...)
 {
-	/*
 	va_list ap;
 	va_start(ap, format);
 	int ret = vswscanf(s, format, ap);
 	va_end(ap);
 	return ret;
-	*/
-	(void)s; (void)format;
-	return -1;
 }
 
 /*
