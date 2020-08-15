caaf42673377d87b05381d6c83eb0d073c919d4b
Jakob Kaivo <jkk@ung.org>
2020-08-14 20:18:19 -0400

    rought draft

--- src/sys/wait/wait3.c
+++ src/sys/wait/wait3.c
@@ -1,8 +1,19 @@
+#include "sys/types.h"
+#include "sys/resource.h"
 #include <sys/wait.h>
 
-pid_t wait3(int *state_loc, int options, struct rusage *resource_usage)
+pid_t wait3(int *stat_loc, int options, struct rusage *resource_usage)
 {
-	return -1;
+	pid_t ret = waitpid((pid_t)-1, stat_loc, options);
+	if (ret == (pid_t)-1) {
+		return (pid_t)-1;
+	}
+
+	if (getrusage(RUSAGE_CHILDREN, resource_usage) == -1) {
+		return (pid_t)-1;
+	}
+
+	return ret;
 }
 
 /*
