902687a68398963b18e5dbed78553c3c19408eeb
Jakob Kaivo <jkk@ung.org>
2020-08-14 20:19:21 -0400

    rough draft

--- src/sys/wait/waitid.c
+++ src/sys/wait/waitid.c
@@ -1,8 +1,21 @@
+#include "sys/types.h"
+/*
+#include "sys/time.h"
+#include "ucontext.h"
+#include "signal.h"
 #include <sys/wait.h>
+*/
+#include "_syscall.h"
+
+#define __SYS_WAIT_H__
+
+#include "idtype_t.c"
+
+typedef struct siginfo siginfo_t;
 
 int waitid(idtype_t idtype, id_t id, siginfo_t *infop, int options)
 {
-	return 0;
+	SYSCALL(waitid, int, -1, idtype, id, infop, option, NULL, 0);
 }
 
 /*
