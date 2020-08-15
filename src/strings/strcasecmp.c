f4478fca7deea454ae169e2d65db2114475a279d
Jakob Kaivo <jkk@ung.org>
2020-08-15 15:42:58 -0400

    implement

--- src/strings/strcasecmp.c
+++ src/strings/strcasecmp.c
@@ -1,8 +1,23 @@
 #include <strings.h>
+#include <ctype.h>
 
 int strcasecmp(const char *s1, const char *s2)
 {
-  return strcasecmp_l (s1, s2, (locale_t)0);
+	while (*s1 && *s2) {
+		char c1 = tolower(*s1);
+		char c2 = tolower(*s2);
+		if (c1 != c2) {
+			return c1 - c2;
+		}
+	}
+
+	if (*s1) {
+		return -1;
+	} else if (*s2) {
+		return 1;
+	}
+
+	return 0;
 }
 
 /*
--- src/strings/strncasecmp.c
+++ src/strings/strncasecmp.c
@@ -1,8 +1,28 @@
 #include <strings.h>
+#include <ctype.h>
 
 int strncasecmp(const char *s1, const char *s2, size_t n)
 {
-  return strncasecmp_l (s1, s2, n, (locale_t)0);
+	while (n > 0 && *s1 && *s2) {
+		char c1 = tolower(*s1);
+		char c2 = tolower(*s2);
+		if (c1 != c2) {
+			return c1 - c2;
+		}
+		n--;
+	}
+
+	if (n == 0) {
+		return 0;
+	}
+
+	if (*s1) {
+		return -1;
+	} else if (*s2) {
+		return 1;
+	}
+
+	return 0;
 }
 
 /*
