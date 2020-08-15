5cfd29ec62db8c2ed6798badb36a71ce1062c1da
Jakob Kaivo <jkk@ung.org>
2020-08-15 16:38:23 -0400

    fix broken line

--- src/_perthread.h
+++ src/_perthread.h
@@ -1,8 +1,7 @@
 #ifndef ___PERTHREAD_H__
 #define ___PERTHREAD_H__
 
-#if defined __STDC_VERSION__ && 201112L <= __STDC_VERSION__ && !defined __STDC_N
-O_THREADS__
+#if defined __STDC_VERSION__ && 201112L <= __STDC_VERSION__ && !defined __STDC_NO_THREADS__
 #define THREAD_LOCAL static _Thread_local
 #else
 #define THREAD_LOCAL static
