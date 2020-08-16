089ecbeb151f896cbe5ef5d3549476d02c6fca78
Jakob Kaivo <jkk@ung.org>
2020-08-16 18:24:02 -0400

    force definition of S_IFIFO

--- src/sys/stat/mkfifo.c
+++ src/sys/stat/mkfifo.c
@@ -1,15 +1,16 @@
-#include "sys/types.h"
+#ifndef _XOPEN_SOURCE
+#define _XOPEN_SOURCE 1
+#endif
+
+#include <sys/types.h>
 #include <sys/stat.h>
 #include "_syscall.h"
-
-#ifndef S_IFIFO
-#include "S_IFIFO.c"
-#endif
 
 int mkfifo(const char *path, mode_t mode)
 {
 	SYSCALL(mknod, int, -1, path, mode, S_IFIFO, 0, 0, 0);
 }
+
 /*
 POSIX(1)
 */
