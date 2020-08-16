734d7730bc1012e2ad008a669e6d91bc55acabe6
Jakob Kaivo <jkk@ung.org>
2020-08-16 18:56:50 -0400

    force defition of MAP_FAILED

--- src/sys/mman/mmap.c
+++ src/sys/mman/mmap.c
@@ -1,10 +1,11 @@
-#include "sys/types.h"
+#if ((!defined _POSIX_C_SOURCE) || (_POSIX_C_SOURCE < 199309L))
+#undef _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 199309L
+#endif
+
+#include <sys/types.h>
 #include <sys/mman.h>
 #include "_syscall.h"
-
-#ifndef MAP_FAILED
-#include "MAP_FAILED.c"
-#endif
 
 void *mmap(void *addr, size_t len, int prot, int flags, int fildes, off_t off)
 {
