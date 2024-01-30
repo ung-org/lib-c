620f2dca70f68748c78e9caa634606a56e8b57b4
Jakob Kaivo <jkk@ung.org>
2024-01-30 13:00:19 -0500

    include proper standard and safety checks

--- src/assert/__assert.c
+++ src/assert/__assert.c
@@ -1,10 +1,11 @@
-#if 0
-
 #include <stdio.h>
 #include <stdlib.h>
+#include "_safety.h"
 
 void __assert(const char *expr, const char *file, int line, const char *func)
 {
+	SIGNAL_SAFE(0);
+
         if (func) {
                 fprintf(stderr, "Assertion failed: %s (%s:%d:%s())\n", expr,
                         file, line, func);
@@ -12,8 +13,10 @@ void __assert(const char *expr, const char *file, int line, const char *func)
                 fprintf(stderr, "Assertion failed: %s (%s:%d)\n", expr, file,
                         line);
         }
+
         abort();
 }
 
-
-#endif
+/*
+STDC(0)
+*/
--- src/assert/assert.c
+++ src/assert/assert.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include "stdlib/_stdlib.h"
 
-_Noreturn void assert(int exp)
+void assert()
 {
-	(void)exp;
 	__stdlib.constraint_handler("Undefined Behavior: The assert() macro has been suppressed to access an actual function", NULL, 0);
 	abort();
 }
 
 /*
+SIGNAL_SAFE(0)
 STDC(0)
 */
