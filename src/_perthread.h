ab3c589cd14c2b7efd79c89fd75ea35edcf9edff
Jakob Kaivo <jkk@ung.org>
2024-01-31 12:24:24 -0500

    disable TSS until it is implemented

--- src/_perthread.h
+++ src/_perthread.h
@@ -1,7 +1,7 @@
 #ifndef ___PERTHREAD_H__
 #define ___PERTHREAD_H__
 
-#if defined __STDC_VERSION__ && 201112L <= __STDC_VERSION__ && !defined __STDC_NO_THREADS__
+#if 0 && (defined __STDC_VERSION__ && 201112L <= __STDC_VERSION__ && !defined __STDC_NO_THREADS__)
 #define THREAD_LOCAL static _Thread_local
 #else
 #define THREAD_LOCAL static
